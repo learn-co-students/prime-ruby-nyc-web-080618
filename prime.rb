911e64a36248ba9e6b052f54b572390fbf80fc5b
Johnny Soong <johnnysoong@hotmail.com>
2018-07-31 14:38:24 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+def prime?(int)
+  array = Array (2...int)
+  value = true
+  if int < 2
+    value = false
+  elsif int == 2
+    value
+  else 
+  index = 0
+  while index < array.size
+  if int % array[index] == 0
+  value = false
+  end
+  index += 1
+end
+end
+value
+end
+
